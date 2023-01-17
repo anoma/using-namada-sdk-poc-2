5765e47a527e9bcbf3ea7fe4d8a040a8444eb5a3
Memas Deligeorgakis <deligeorgakismemas@googlemail.com>
2023-01-17 14:04:16 +0100

    Added a comment

--- src/sdk_wrapper/all_platforms_store/mod.rs
+++ src/sdk_wrapper/all_platforms_store/mod.rs
@@ -11,6 +11,11 @@ impl DesktopStore {
     /// if there is an existing storage, it is being retrieved, possibly decrypted
     /// If none existed before a new one is being initialized and returned
     pub fn load_or_initialize() -> Store {
+        // way 1 - always create storage based on data from browser
+        // and augment store and provide it for the SDK
+
+        // way 2 - deserialize the persisted store and possibly sync the web client
+        // and also other way from client to store
         let store = Store::default();
         store
     }
